2ca2142282a5d9c2af638294c02c7e23b3a81e4d
Emmanuel222-coder <emmanuelosoro52@gmail.com>
2025-10-09 06:27:13 +0300

    Add files via upload

---
+++ for_loop.c
@@ -0,0 +1,19 @@
+//programming
+/*
+Name; EMMANUEL OSORO
+Registeration number: PA106/G/28823/25
+Description:A FOR LOOP TO DISPLAY NUMBERS DESCENDING FROM 100 TO 50
+*/
+
+#include<stdio.h>
+
+int main(){
+	//variable declaration
+	int i;
+	
+	for(i=100; i>=50; i--)
+	{
+		printf("%d\n", i);
+	}
+	return 0;
+}
